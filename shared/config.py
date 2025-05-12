ee7f5e595f2e50f97d4de296b632d8783fe52631
josedrojasa <josedrojasa@hotmail.com>
2025-05-11 21:46:01 -0500

    ignore env file

--- shared/config.py
+++ shared/config.py
@@ -3,6 +3,6 @@ from pydantic_settings import BaseSettings
 
 class Config(BaseSettings):
     class Config:
-        env_file = ".env"
-        env_file_encoding = 'utf-8'
+        #env_file = ".env"
+        #env_file_encoding = 'utf-8'
         secrets_dir = None # Desactiva cualquier caché de secretos
