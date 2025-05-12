1fad3cf352e3bea109dcf3a5e079e873450c8537
josedrojasa <josedrojasa@hotmail.com>
2025-05-11 22:40:40 -0500

    add lambdas

--- cdk/cdk/cdk_stack.py
+++ cdk/cdk/cdk_stack.py
@@ -1,19 +1,30 @@
 from aws_cdk import (
-    # Duration,
     Stack,
-    # aws_sqs as sqs,
+    aws_lambda
 )
+from aws_cdk.aws_lambda_python_alpha import PythonFunction
+
 from constructs import Construct
 
 class CdkStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
+        createInvoiceLambda = PythonFunction(
+            self,
+            "createInvoiceLambda",
+            entry="./lambda/create_invoice",
+            runtime=aws_lambda.Runtime.PYTHON_3_10,
+            index="index.py",
+            handler="handle"
+        )
+        sendTestLambda = PythonFunction(
+            self,
+            "sendTestLambda",
+            entry="./lambda/send_test",
+            runtime=aws_lambda.Runtime.PYTHON_3_10,
+            index="index.py",
+            handler="handle"
+        )
 
-        # The code that defines your stack goes here
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "CdkQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
